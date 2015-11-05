f05feef0df7994f561599441f63ebd9ed331c6b8
Ben Smith <benjsmi@us.ibm.com>
2015-11-05 17:28:42 -0500

    Whoops; forgot the scheme in the url.

--- player-wlpcfg/startup.sh
+++ player-wlpcfg/startup.sh
@@ -2,6 +2,6 @@
 export DOCKERHOST=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
 echo Found Docker host: $DOCKERHOST
 export MONGO_HOST=$DOCKERHOST
-export CONCIERGE_URL=$DOCKERHOST:9081/concierge
+export CONCIERGE_URL=http:\/\/$DOCKERHOST:9081/concierge
 /opt/ibm/wlp/bin/server run defaultServer
 
