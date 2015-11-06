bba39d9dc7a33d3c077646259985feeb53590129
Ben Smith <benjsmi@us.ibm.com>
2015-11-06 08:54:33 -0500

    Use the proxy's private IP so that traffic goes through the host-only network, but can still be load-balanced.

--- player-wlpcfg/startup.sh
+++ player-wlpcfg/startup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-export DOCKERHOST=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
-echo Found Docker host: $DOCKERHOST
-export MONGO_HOST=$DOCKERHOST
-export CONCIERGE_URL=http:\/\/$DOCKERHOST:9081/concierge
 /opt/ibm/wlp/bin/server run defaultServer
 
