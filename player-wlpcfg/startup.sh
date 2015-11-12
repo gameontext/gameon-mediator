25bf2a3c0682ed7ecd121ac90d28e2ba9fa31454
Ben Smith <benjsmi@us.ibm.com>
2015-11-12 12:33:12 -0500

    Give logmet a go for the player service.

--- player-wlpcfg/startup.sh
+++ player-wlpcfg/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-/opt/ibm/wlp/bin/server run defaultServer
-
+/opt/ibm/wlp/bin/server start defaultServer
+echo Running Logstash Forwarder...
+sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /opt/forwarder.conf
+cd /opt ; ./forwarder --config ./forwarder.conf
