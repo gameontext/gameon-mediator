ab421fe583cc26bfe3275913675802160abf18f7
Erin Schnabel <ebullient@users.noreply.github.com>
2015-12-04 16:31:58 -0500

    Updates for IDS pipeline

--- idsBuild.sh
+++ idsBuild.sh
@@ -13,25 +13,15 @@ echo Downloading Docker requirements..
 wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/dockerneeds.tar -q
 echo Setting up Docker...
 tar xzf dockerneeds.tar
-cd .. 
+cd ..
 
-echo Downloading Java 8...
-wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/jdk-8u65-x64.tar.gz -q
-echo Extracting Java 8...
-tar xzf jdk-8u65-x64.tar.gz
-echo And removing the tarball...
-rm jdk-8u65-x64.tar.gz
-export JAVA_HOME=$(pwd)/jdk1.8.0_65
 echo Building projects using gradle...
-./gradlew build 
-echo Building and Starting Concierge Docker Image...
+./gradlew build
+echo Building and Starting Mediator Docker Image...
 cd mediator-wlpcfg
 sed -i s/PLACEHOLDER_ADMIN_PASSWORD/$ADMIN_PASSWORD/g ./Dockerfile
 
-../gradlew buildDockerImage 
-../gradlew stopCurrentContainer 
+../gradlew buildDockerImage
+../gradlew stopCurrentContainer
 ../gradlew removeCurrentContainer
 ../gradlew startNewContainer
-echo Removing JDK, cause there's no reason that's an artifact...
-cd ..
-rm -rf jdk1.8.0_65
--- mediator-wlpcfg/startup.sh
+++ mediator-wlpcfg/startup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-/opt/ibm/wlp/bin/server start defaultServer
-echo Running Logstash Forwarder...
-sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /opt/forwarder.conf
-cd /opt ; ./forwarder --config ./forwarder.conf
+
+if [ "$LOGSTASH_ENDPOINT" != "" ]; then
+  /opt/ibm/wlp/bin/server start defaultServer
+  echo Starting the logstash forwarder...
+  sed -i s/PLACEHOLDER_LOGHOST/$LOGSTASH_ENDPOINT/g /opt/forwarder.conf
+  cd /opt
+  chmod +x ./forwarder
+  echo -e $LOGSTASH_CERT > logstash-forwarder.crt
+  echo -e $LOGSTASH_KEY > logstash-forwarder.key
+  sleep 0.5
+  ./forwarder --config ./forwarder.conf
+else
+  /opt/ibm/wlp/bin/server run defaultServer
+fi
