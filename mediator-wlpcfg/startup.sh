5ec7f5f5ecff55265541f61a0d662426b4d662b1
Ben Smith <benjsmi@us.ibm.com>
2015-12-11 11:14:02 -0500

    Add SSL cert to Liberty Keystore.

--- mediator-wlpcfg/startup.sh
+++ mediator-wlpcfg/startup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ "$SSL_CERT" != "" ]; then
+  echo Found an SSL cert to use.
+  cd /opt/ibm/wlp/usr/servers/defaultServer/resources/
+  echo -e $SSL_CERT > cert.pem
+  openssl pkcs12 -passin pass:keystore -passout pass:keystore -export -out cert.pkcs12 -in cert.pem
+  keytool -import -v -trustcacerts -alias default -file cert.pem -storepass truststore -keypass keystore -noprompt -keystore security/truststore.jks
+  keytool -genkey -storepass testOnlyKeystore -keypass wefwef -keyalg RSA -alias endeca -keystore security/key.jks -dname CN=rsssl,OU=unknown,O=unknown,L=unknown,ST=unknown,C=CA
+  keytool -delete -storepass testOnlyKeystore -alias endeca -keystore security/key.jks
+  keytool -v -importkeystore -srcalias 1 -alias 1 -destalias default -noprompt -srcstorepass keystore -deststorepass testOnlyKeystore -srckeypass keystore -destkeypass testOnlyKeystore -srckeystore cert.pkcs12 -srcstoretype PKCS12 -destkeystore security/key.jks -deststoretype JKS
+fi
+
 if [ "$LOGSTASH_ENDPOINT" != "" ]; then
   /opt/ibm/wlp/bin/server start defaultServer
   echo Starting the logstash forwarder...
