65f5ba4e9b9b1fa6e4f9e4128b17516e2b80af87
Ozzy Osborne <ozzy@ca.ibm.com>
2016-05-27 16:54:54 -0400

    add message hub jar and kafka etcd to startup.sh
    Signed-off-by: Ozzy Osborne <ozzy@ca.ibm.com>

--- mediator-wlpcfg/startup.sh
+++ mediator-wlpcfg/startup.sh
@@ -45,6 +45,16 @@ if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   export LOGMET_TENANT=$(etcdctl get /logmet/tenant)
   export LOGMET_PWD=$(etcdctl get /logmet/pwd)
   export SYSTEM_ID=$(etcdctl get /player/system_id)
+  export KAFKA_URL=$(etcdctl get /kafka/url)
+  export KAFKA_USER=$(etcdctl get /kafka/user)
+  export KAFKA_PASSWORD=$(etcdctl get /passwords/kafka)
+
+  #to run with message hub, we need a jaas jar we can only obtain
+  #from github, and have to use an extra config snippet to enable it.
+  cd /opt/ibm/wlp/usr/servers/defaultServer
+  mkdir -p configDropins/overrides
+  mv kafkaDropin.xml configDropins/overrides
+  wget https://github.com/ibm-messaging/message-hub-samples/raw/master/java/message-hub-liberty-sample/lib-message-hub/messagehub.login-1.0.0.jar
 
   # Softlayer needs a logstash endpoint so we set up the server
   # to run in the background and the primary task is running the
