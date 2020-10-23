1a20bb0293617b3bbcf49a79943d8de653748775
Erin Schnabel <schnabel@us.ibm.com>
2020-10-23 15:29:03 -0400

    Update SSL cert management

--- mediator-wlpcfg/startup.sh
+++ mediator-wlpcfg/startup.sh
@@ -37,26 +37,7 @@ if [ -f /etc/cert/cert.pem ]; then
   cp /etc/cert/cert.pem ${ssl_path}/cert.pem
 fi
 
-if [ -f ${ssl_path}/cert.pem ] ; then
-  echo "Building keystore/truststore from cert.pem"
-  echo "-creating dir"
-  echo "-cd dir"
-  cd ${ssl_path}
-  echo "-converting pem to pkcs12"
-  openssl pkcs12 -passin pass:keystore -passout pass:keystore -export -out cert.pkcs12 -in cert.pem
-  echo "-importing pem to truststore.jks"
-  keytool -import -v -trustcacerts -alias default -file cert.pem -storepass truststore -keypass keystore -noprompt -keystore truststore.jks
-  echo "-creating dummy key.jks"
-  keytool -genkey -storepass testOnlyKeystore -keypass wefwef -keyalg RSA -alias endeca \
-          -keystore key.jks -dname CN=rsssl,OU=unknown,O=unknown,L=unknown,ST=unknown,C=CA
-  echo "-emptying key.jks"
-  keytool -delete -storepass testOnlyKeystore -alias endeca -keystore key.jks
-  echo "-importing pkcs12 to key.jks"
-  keytool -v -importkeystore -srcalias 1 -alias 1 -destalias default -noprompt \
-          -srcstorepass keystore -deststorepass testOnlyKeystore -srckeypass keystore -destkeypass testOnlyKeystore \
-          -srckeystore cert.pkcs12 -srcstoretype PKCS12 -destkeystore key.jks -deststoretype JKS
-  echo "done"
-  cd ${SERVER_PATH}
-fi
+# Make sure keystores are present or are generated
+/opt/gen-keystore.sh ${ssl_path} ${ssl_path}
 
 exec /opt/ol/wlp/bin/server run defaultServer
